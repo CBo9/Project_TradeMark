59f56038b796ed09da248f0ac37e78fe235c0e4c
CBo9 <clement.boilevin@gmail.com>
2021-01-14 23:40:08 +0100

    fix: snake-case names converted to camel-case for hydration

--- class/Generic.php
+++ class/Generic.php
@@ -4,6 +4,13 @@ class Generic{
 
 	protected function hydrate(array $data){
 		foreach ($data as $key => $value) {
+			if (preg_match('/_/u', $key)){
+			    $words = explode("_", $key);
+			    foreach ($words as $key => $word) {
+			        $words[$key] = ucfirst($word);
+			    }
+			    $key = implode($words);
+			}
 			$method = "set" . ucfirst($key);
 			if(method_exists($this, $method)){
 				$this->$method($value);
